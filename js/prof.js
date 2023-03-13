8afea44d2e82d18ccb061376da8bb9402d2f5d82
vagner-jr <vagnerflorindojunior@gmail.com>
2023-03-13 09:18:38 -0300

    add condition in score total

--- js/prof.js
+++ js/prof.js
@@ -1,11 +1,24 @@
 const button = document.getElementById('btn-new')
 const modal = document.querySelector('dialog')
-const buttonClose = document.getElementById('btn-close')
 
 button.addEventListener('click', () => {
     modal.showModal()
 })
 
-buttonClose.addEventListener('click', () => {
+const  frm = document.querySelector('dialog form')
+const cardManName = document.querySelector('.man .info h3')
+const cardManSubject = document.querySelector('.man .info p')
+const cardWomanName = document.querySelector('.woman .info h3')
+const cardWomanSubject = document.querySelector('.woman .info p')
+
+frm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const name = frm.inName.value;
+    const subject = frm.inSubject.value;
+
+    cardName.innerHTML = name
+    cardSubject.innerHTML = `Professor de ${subject}`
+
     modal.close()
 })
---
+++ js/score.js
@@ -0,0 +1,50 @@
+const frm = document.querySelector("form");
+const studentTable = document.querySelector(".student-total");
+
+const students = [];
+frm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const name = frm.inName.value;
+  const score1 = Number(frm.inScore1.value);
+  const score2 = Number(frm.inScore2.value);
+  const total = (score1 + score2) / 2;
+
+  students.push({ name, score1, score2, total });
+  let list = "";
+  for (let i = 0; i < students.length; i++) {
+    if (students[i].total < 4) {
+      list += `      
+      <ul class="row">
+        <li class="col">${students[i].name}</li>
+        <li class="col">${students[i].score1}</li>
+        <li class="col">${students[i].score2}</li>
+        <li class="col" style="color: rgba(255, 0, 0, 0.735);">${students[i].total}</li>
+      </ul>`;
+    } else {
+      if (students[i].total >= 4 && students[i].total < 6) {
+        list += `      
+        <ul class="row">
+          <li class="col">${students[i].name}</li>
+          <li class="col">${students[i].score1}</li>
+          <li class="col">${students[i].score2}</li>
+          <li class="col" style="color: rgb(21, 168, 21);">${students[i].total}</li>
+        </ul>`;
+      } else {
+        list += `      
+        <ul class="row">
+          <li class="col">${students[i].name}</li>
+          <li class="col">${students[i].score1}</li>
+          <li class="col">${students[i].score2}</li>
+          <li class="col" style="color: #0d80d4;">${students[i].total}</li>
+        </ul>`;
+      }
+    }
+  }
+
+  studentTable.innerHTML = list;
+
+  frm.inName.value = "";
+  frm.inName.focus();
+  frm.inScore1.value = "";
+  frm.inScore2.value = "";
+});
